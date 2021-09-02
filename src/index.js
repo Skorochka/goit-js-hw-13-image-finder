0770917c6bbd41606b1897c4445efb40d0bb76e0
Skorochka <79852838+Skorochka@users.noreply.github.com>
2021-09-03 00:09:09 +0300

    Создает рендер элементов на страницу

--- src/index.js
+++ src/index.js
@@ -1,58 +1,94 @@
-import NewApi from './js/apiService.js'
+import '@pnotify/core/dist/BrightTheme.css'
+import { error } from '../node_modules/@pnotify/core/dist/PNotify.js'
+import fetchImg from './js/apiService.js'
 import templateMarkup from './templates/templateMarkup.hbs'
+import * as basicLightbox from 'basiclightbox'
 
-const newApi = new NewApi()
 
-const refs = {
+
+export const refs = {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
     loadMore: document.querySelector('.load-more-btn'),
-
+    conteiner: document.querySelector('.largeImage-conteiner'),
+    largeImg: document.querySelector('.largeImage-img'),
+    closeBtn: document.querySelector('.close-btn')
 }
 
-refs.form.addEventListener('submit', onSearch),
-refs.loadMore.addEventListener('click', onLoadMoreBtnClick)
+refs.form.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMoreBtnClick);
+refs.gallery.addEventListener('click', onImgClick)
 
+    
+export let page = 1;
+        let searchQuery
 
 function onSearch(e) {
     e.preventDefault()
-    newApi.query = e.currentTarget.elements.query.value
-    newApi.resetPage()
-    newApi.fetchImg().then(data =>
-    {
-        clearGallery()
-        appendMarkup(data)
-        })
+    searchQuery = e.currentTarget.elements.query.value;
+    resetPage()
+    clearGallery()
+    e.currentTarget.elements.query.value = ''
     
+
+    renderCollection()
+
 }
 
+function renderCollection() {
+    fetchImg(searchQuery).then(data => {
+        incrementPage()
+        appendMarkup(data)
+    }).catch(err => {
+      error({ text: err })
+    })
+}
+
+function incrementPage() {
+        page += 1;
+    }
+
+function resetPage() {
+        page = 1;
+    }
 
 function appendMarkup(data) {
     refs.gallery.insertAdjacentHTML('beforeend', templateMarkup(data))
-    
 }
 
 function onLoadMoreBtnClick() {
-    newApi.fetchImg().then(data => {
-        scroll();
-        appendMarkup(data)
-        })
+    renderCollection()
     }
-
-
 
 function clearGallery() {
     refs.gallery.innerHTML = ''
 }
 
-function scroll() {
-    refs.gallery.scrollIntoView(
-    {
-        behavior: 'smooth',
-        block: 'end',
-    }
-)
+function onImgClick(e) {
+    const url = e.target.dataset.img
+
+    console.log(url)
+
+    refs.conteiner.classList.add('is-open')
+    refs.largeImg.setAttribute('src', url)
+    
+   
 }
+
+refs.closeBtn.addEventListener('click', onCloseBtnClick)
+
+function onCloseBtnClick() {
+    refs.conteiner.classList.remove('is-open')
+        refs.largeImg.src = ''
+
+
+}
+
+
+
+
+
+
 
 
 
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,60 +1,11 @@
-export default class NewApi {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.API_KEY = `23195442-5432850696f797ca09021b772`;
+import { page } from '../index.js'
 
 
-    }
-
-    fetchImg() {
-    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.API_KEY}`)
-    .then(r => r.json())
-        .then(data => {
-            this.incrementPage()
-            return data.hits
-        })
-    .catch(err => console.log(err))
-    }
-
-    incrementPage() {
-        this.page += 1;
-
-    }
-
-    resetPage() {
-        this.page = 1;
-    }
-
-    get query() {
-        return this.searchQuery
-    }
-    set query(newQuery) {
-        this.searchQuery = newQuery
-    }
-    
+export default function fetchImg(searchQuery) {
+        const API_KEY = `23195442-5432850696f797ca09021b772`;
+ return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`)
+    .then(r => r.json()).then(promise => promise.hits)
 }
 
 
 
-
-
-
-// Тебе интересны следующие свойства:
-
-// webformatURL - ссылка на маленькое изображение для списка карточек
-// largeImageURL - ссылка на большое изображение (смотри пункт 'дополнительно')
-// likes - количество лайков
-// views - количество просмотров
-// comments - количество комментариев
-// downloads - количество загрузок
-
-
-
-
-
-// const element = document.getElementById('.my-element-selector');
-// element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
